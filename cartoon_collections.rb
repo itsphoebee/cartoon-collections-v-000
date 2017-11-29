90c67f9ba37c998b99acbdc6eababe5ae0efe582
Phoebe Lau <phoebelau7@gmail.com>
2017-11-29 16:20:41 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,7 +5,7 @@ end
 
 def summon_captain_planet(summons)# code an argument here
   # Your code here
-  summons.upcase.join("!").map
+  summons.split.upcase.join("!").map
 end
 
 def long_planeteer_calls# code an argument here
