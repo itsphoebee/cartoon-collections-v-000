26a4aa85aef6841439e509b398574b30cd915a4b
Phoebe Lau <phoebelau7@gmail.com>
2017-11-29 16:36:10 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,8 +1,8 @@
 def roll_call_dwarves(list)# code an argument here
   # Your code here
-  list.each_with_index do |dwarf, order| 
+  list.each_with_index do |dwarf, order|
     puts "#{dwarf}" puts "#{order}"
-
+  end
 end
 
 def summon_captain_planet(summons)# code an argument here
