f6eb1220e3c5d8a7933881665da2b4ad5afb1770
Phoebe Lau <phoebelau7@gmail.com>
2017-11-29 16:37:26 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,7 +1,7 @@
 def roll_call_dwarves(dwarves)# code an argument here
   # Your code here
   dwarves.each_with_index do |dwarf, order|
-    puts "#{dwarf}" puts "#{order}"
+    puts "#{order}. #{dwarf}"
   end
 end
 
