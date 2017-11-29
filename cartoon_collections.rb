7ba71826ad3d4bacf4353c30b9aab810498d7d62
Phoebe Lau <phoebelau7@gmail.com>
2017-11-29 17:03:01 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -19,6 +19,6 @@ end
 def find_the_cheese(all_cheeses)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
-  all_cheeses.find do 
+
 
 end
