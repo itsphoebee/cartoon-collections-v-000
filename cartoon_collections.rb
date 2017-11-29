756c963bd405ebcd570807b8713b6e8fb77a228e
Phoebe Lau <phoebelau7@gmail.com>
2017-11-29 16:40:47 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,9 +14,9 @@ def long_planeteer_calls(calls)# code an argument here
   # Your code here
   calls.find do |call|
     if call.size > 4
-      return false
+      return true
     else
-      true
+      false
     end
   end
 end
